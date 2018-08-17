49584215011ee2dd0c31ef5e8027bd2363468101
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-08-16 22:23:22 -0700

    better redirects

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/cart.html', function(req, res, next) {
+  res.redirect('/cart');
+});
+
+router.get('/cart', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 module.exports = router;
