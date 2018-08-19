342bc7d0262621c634dde381a2e2ea0754a5b26e
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-08-18 21:01:42 -0700

    trending products shows on site

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,7 +11,26 @@ router.get('/', function(req, res, next) {
   console.log(process.env.API_KEY);
   fetch(`http://api.walmartlabs.com/v1/trends?format=json&apiKey=${process.env.API_KEY}`)
       .then(res => res.json())
-      .then(json => res.render('index', { title: 'Deepest Discounts', json: json.items[0].name }));
+      .then(json => {
+        let products = [];
+        for (let i = 0; i < 12; i++) {
+          let product = {
+            itemID: json.items[i].itemID,
+            name: json.items[i].name,
+            msrp: json.items[i].msrp,
+            salePrice: json.items[i].salePrice,
+            thumbnailImage: json.items[i].thumbnailImage,
+            mediumImage: json.items[i].mediumImage,
+            largeImage: json.items[i].largeImage,
+            productTrackingUrl: json.items[i].productTrackingUrl,
+            productUrl: json.items[i].productUrl
+          }
+          products.push(product)
+        }
+        console.log(products);
+
+        res.render('index', { title: 'Deepest Discounts', products: products })
+      });
 });
 
 /* GET home page. */
