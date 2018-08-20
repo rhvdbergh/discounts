7145bdbd219c470f91c011566eb2fe1b0d2fb417
Ronald van der Bergh <rhvdbergh@gmail.com>
2018-08-19 22:33:31 -0700

    calculates msrp - salePrice percentages

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  fetch (`http://api.walmartlabs.com/v1/feeds/rollback?apikey=${process.env.API_KEY}&amp;categoryId=3944`)
+  fetch (`http://api.walmartlabs.com/v1/paginated/items?category=3891&apiKey=${process.env.API_KEY}&format=json`)
     .then(res => res.json())
     .then(json => {
 
@@ -18,24 +18,67 @@ router.get('/', function(req, res, next) {
     });
 
   // displays clearance
-  fetch(`http://api.walmartlabs.com/v1/feeds/bestsellers?apikey=${process.env.API_KEY}&amp;categoryId=3944`)
+  fetch(`http://api.walmartlabs.com/v1/paginated/items?category=3944_3951&apiKey=${process.env.API_KEY}&format=json`)
       .then(res => res.json())
       .then(json => {
         let products = [];
-        for (let i = 0; i < 12; i++) {
-          let product = {
-            itemId: json.items[i].itemId,
-            name: json.items[i].name,
-            msrp: json.items[i].msrp,
-            salePrice: json.items[i].salePrice,
-            thumbnailImage: json.items[i].thumbnailImage,
-            mediumImage: json.items[i].mediumImage,
-            largeImage: json.items[i].largeImage,
-            productTrackingUrl: json.items[i].productTrackingUrl,
-            productUrl: json.items[i].productUrl
+        let viableCount = 0;
+        let totalMSRP = 0;
+        let totalSalePrice = 0;
+        let discountPercentages = [];
+        let numProductsOnScreen = 0;
+
+        for (let i = 0; i < json.items.length; i++) {
+          if ((json.items[i].msrp && json.items[i].salePrice) && json.items[i].availableOnline && (json.items[i].stock === 'Available')) {
+            if ((json.items[i].msrp > json.items[i].salePrice) && (json.items[i].msrp !== 0) && (json.items[i].msrp !== 9999)) {
+              if (100-(json.items[i].salePrice / json.items[i].msrp * 100) >= 50) {
+              viableCount++;
+              totalMSRP += json.items[i].msrp;
+              totalSalePrice+= json.items[i].salePrice;
+              discountPercentages.push(100-(json.items[i].salePrice / json.items[i].msrp * 100));
+
+              if (numProductsOnScreen < 12) {
+                let product = {
+                      itemId: json.items[i].itemId,
+                      name: json.items[i].name,
+                      upc: json.items[i].upc,
+                      msrp: json.items[i].msrp,
+                      salePrice: json.items[i].salePrice,
+                      shortDescription: json.items[i].shortDescription,
+                      thumbnailImage: json.items[i].thumbnailImage,
+                      mediumImage: json.items[i].mediumImage,
+                      largeImage: json.items[i].largeImage,
+                      productTrackingUrl: json.items[i].productTrackingUrl,
+                      productUrl: json.items[i].productUrl
+                }
+                products.push(product);
+                console.log(product.productUrl);
+                numProductsOnScreen++;
+              }
+            }
           }
-          products.push(product)
+          }
         }
+
+        console.log('viableCount', viableCount);
+        console.log('totalMSRP', totalMSRP);
+        console.log('totalSalePrice', totalSalePrice);
+        console.log('average discount in percentages', discountPercentages.reduce((acc, val) => acc + val) / discountPercentages.length);
+
+        // for (let i = 0; i < 12; i++) {
+        //   let product = {
+        //     itemId: json.items[i].itemId,
+        //     name: json.items[i].name,
+        //     msrp: json.items[i].msrp,
+        //     salePrice: json.items[i].salePrice,
+        //     thumbnailImage: json.items[i].thumbnailImage,
+        //     mediumImage: json.items[i].mediumImage,
+        //     largeImage: json.items[i].largeImage,
+        //     productTrackingUrl: json.items[i].productTrackingUrl,
+        //     productUrl: json.items[i].productUrl
+        //   }
+        //   products.push(product)
+        // }
         res.render('index', { title: 'Deepest Discounts', products: products })
       });
 });
